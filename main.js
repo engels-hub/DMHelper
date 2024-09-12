aff0025b7d0930c6264beeae4b80cd809c761629
Artjoms Papšs <68332986+engels-hub@users.noreply.github.com>
2024-09-12 14:21:53 +0300

    drag and drop divs

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
 const { app, BrowserWindow } = require('electron')
 const path = require('node:path')
 
+
 const createWindow = () => {
     const win = new BrowserWindow({
       width: 800,
       height: 600,
       webPreferences: {
+        nodeIntegration:  true,
+        contextIsolation: false,
         preload: path.join(__dirname, 'preload.js')
       }
     })
