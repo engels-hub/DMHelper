a0ca85b0711a38002142c2389fbeaa55d8009c41
Maksims Makarskis 221RDB380 <77271405+Dogma688@users.noreply.github.com>
2024-10-01 16:04:02 +0300

    DiceRolls.js

---
+++ DiceRolls.js
@@ -0,0 +1,55 @@
+function D4Roll() {
+
+    const Dice = Math.floor(Math.random() * 4) + 1;
+
+    console.log("D4 roll result: ", Dice);
+
+}
+
+function D6Roll() {
+
+    const Dice = Math.floor(Math.random() * 6) + 1;
+
+    console.log("D6 roll result: ", Dice);
+
+}
+
+function D8Roll() {
+
+    const Dice = Math.floor(Math.random() * 8) + 1;
+
+    console.log("D8 roll result: ", Dice);
+
+}
+
+function D10Roll() {
+
+    const Dice = Math.floor(Math.random() * 10) + 1;
+
+    console.log("D10 roll result: ", Dice);
+
+}
+
+function D12Roll() {
+
+    const Dice = Math.floor(Math.random() * 12) + 1;
+
+    console.log("D12 roll result: ", Dice);
+
+}
+
+function D20Roll() {
+
+    const Dice = Math.floor(Math.random() * 20) + 1;
+
+    console.log("D20 roll result: ", Dice);
+
+}
+
+function D100Roll() {
+
+    const Dice = Math.floor(Math.random() * 100) + 1;
+
+    console.log("D100 roll result: ", Dice);
+
+}
